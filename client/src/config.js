9acaa4e0849f5116bd3739e10f7981a827bfe9f1
Pegu-Mrigen <mrig8520@gmail.com>
2026-09-16 16:58:36 +0530

    CLIENT END CORRECTED

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,9 @@
-const API_URL = import.meta.env.REACT_APP_SERVER_URL 
-  ? "https://real-estate-at-gohpur-server.vercel.app" 
-  : "http://localhost:5000/api";
+// const API_URL = import.meta.env.REACT_APP_SERVER_URL 
+//   ? "https://real-estate-at-gohpur-server.vercel.app/api" 
+//   : "http://localhost:5000/api";
+
+// export default API_URL;
+const API_URL = "https://real-estate-at-gohpur-server.vercel.app/api"
 
 export default API_URL;
 
