dbabed9db9cbec2ea619f223cebd1ef2a5b12869
Pegu-Mrigen <mrig8520@gmail.com>
2026-09-16 18:38:37 +0530

    vite.config reversed

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,27 +1,10 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(),tailwindcss(),],
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      // Whenever frontend hits '/api', forward it to the Express backend
-      '/api': {
-        target: 'https://real-estate-at-gohpur-server.vercel.app/api',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
+  plugins: [react(),tailwindcss(),],
 })
 
