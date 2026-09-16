eb5f149ea98958484d8df3597ebd2dcb81bb2d15
Pegu-Mrigen <mrig8520@gmail.com>
2026-09-16 18:14:17 +0530

    import.meta.env.VITE_PRODUCTION_URL ADDED

--- client/src/config.js
+++ client/src/config.js
@@ -1,8 +1,13 @@
-const API_URL = import.meta.env.PRODUCTION
-  ? "https://real-estate-at-gohpur-server.vercel.app/api" 
-  : "http://localhost:5000/api";
+// const API_URL = import.meta.env.VITE_PRODUCTION
+//   ? "https://real-estate-at-gohpur-server.vercel.app/api" 
+//   : "http://localhost:5000/api"; 
+const API_URL = import.meta.env.VITE_PRODUCTION
+  ? import.meta.env.VITE_PRODUCTION_URL 
+  : "http://localhost:5000/api"; 
 
   export default API_URL;
+
+  
 
 // export default API_URL;
 // const API_URL = "https://real-estate-at-gohpur-server.vercel.app/api"
